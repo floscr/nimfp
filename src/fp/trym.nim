1966bc290062b11cf8e620a2117087eeb27f83c5
Florian Schroedl <flo.schroedl@gmail.com>
2022-06-07 22:33:04 +0200

    Implement Try/tap

--- src/fp/trym.nim
+++ src/fp/trym.nim
@@ -78,6 +78,26 @@ proc fold*[A,B](v: Try[A], ifFailure: ref Exception -> B, ifSuccess: A -> B): B 
   else:
     ifSuccess(v.get())
 
+proc tap*[A](v: Try[A], ifSuccess: A -> void): Try[A] =
+  ## Applies side-effect `ifSuccess` and returns orignal result `v`
+  if v.isSuccess():
+    ifSuccess(v.get())
+  v
+
+proc tapErr*[A](v: Try[A], ifFailure: ref Exception -> void): Try[A] =
+  ## Applies side-effect `ifFailure` and returns orignal result `v`
+  if v.isErr():
+    ifFailure(v.getError())
+  v
+
+proc bitap*[A](v: Try[A], ifFailure: ref Exception -> void, ifSuccess: A -> void): Try[A] =
+  ## Applies side-effect `ifFailure` or `ifSuccess` and returns orignal result `v`
+  if v.isSuccess():
+    ifSuccess(v.get())
+  else:
+    ifFailure(v.getError())
+  v
+
 proc getErrorStack*[A](v: Try[A]): string =
   ## Returns the exception stack trace
   v.getError.getStackTrace
@@ -115,3 +135,12 @@ when isMainModule:
   assert tryET("F".parseInt()).fold((x: ref Exception) => "Fail", (x: int) => "Suc") == "Fail"
   assert "Suc".success().fold((x: ref Exception) => x.msg, (x: string) => x) == "Suc"
   assert "Fail".failure(string).fold((x: ref Exception) => x.msg, (x: string) => x) == "Fail"
+
+  # tap
+  var mutBitapOk: int
+  var mutBitapErr: string
+  assert tryET("1".parseInt()).bitap((x: ref Exception) => (mutBitapErr = x.msg), (x: int) => (mutBitapOk = x)) == 1.success()
+  assert mutBitapOk == 1 and mutBitapErr == ""
+  mutBitapOk = 0
+  assert tryET("Fail".parseInt()).bitap((x: ref Exception) => (mutBitapErr = "Error"), (x: int) => (mutBitapOk = x)).isFailure()
+  assert mutBitapOk == 0 and mutBitapErr == "Error"
